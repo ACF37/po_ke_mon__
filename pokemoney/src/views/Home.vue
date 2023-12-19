2c62a934ca2a21af822c54766ad50f2aa9233698
ACF37 <64457364+ACF37@users.noreply.github.com>
2023-12-19 09:58:13 +0900

    サンプルの追加

---
+++ pokemoney/src/views/Home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+<h1>{{ count }}</h1>
+<button type="button" @click="count++">Count up</button>
+</template>
