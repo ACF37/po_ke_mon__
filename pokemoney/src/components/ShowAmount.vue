0befb344fe2552d93384185fbaa653829242d780
ACF37 <64457364+ACF37@users.noreply.github.com>
2023-12-20 03:39:51 +0000

    表示コンポーネントの作成

---
+++ pokemoney/src/components/ShowAmount.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+defineProps<{ label: string, amount: number }>()
+</script>
+
+<template>
+<div>
+    <h4>{{ label }}</h4>
+    <span>{{ amount }}</span>
+</div>
+</template>
