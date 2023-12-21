af2bb6bf9d75942b9b7e8af8d839f20effdf7fcc
ACF37 <64457364+ACF37@users.noreply.github.com>
2023-12-21 23:43:41 +0000

    CSS追加

--- pokemoney/src/components/InputNumber.vue
+++ pokemoney/src/components/InputNumber.vue
@@ -1,19 +1,37 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-const emit = defineEmits(['submitnum'])
+const emit = defineEmits(['submitnum','close'])
 
-const amount = ref(0)
+const amount = ref()
 
 function submit() {
-    emit('submitnum', amount)
+    emit('submitnum', amount.value)
 }
 </script>
 
 <template>
-    <div class="overlay">
+    <div class="overlay" @click.self="emit('close')">
         <div class="modal">
             <input type="number" v-model="amount"/>
             <button @click="submit">Submit</button>
         </div>
     </div>
 </template>
+<style scoped>
+.overlay {
+    position: fixed;
+    z-index: 1;
+    background-color: #00000055;
+    height: 100vh;
+    width: 100%;
+    inset: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.modal{
+    padding: 3rem;
+    background-color: black;
+}
+
+</style>
